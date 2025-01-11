13a6951093fe9e28a136061fe2f51fda79835cc9
koreteau <cortocolloc@gmail.com>
2025-01-11 17:49:11 +0100

    Update LoadData.jsx

--- app/src/components/Applications/Capaci/LoadData.jsx
+++ app/src/components/Applications/Capaci/LoadData.jsx
@@ -4,7 +4,7 @@ import { LoadDataCurrencyRates } from "./LoadDataCurrencyRates";
 export function DataLoad() {
     return (
         <div className="p-4">
-            <h1 className="text-lg font-bold mb-4">Data Load</h1>
+            <h1 className="text-lg font-bold mb-4">Currency Load</h1>
             <div className="mt-4">
                 <LoadDataCurrencyRates />
             </div>
