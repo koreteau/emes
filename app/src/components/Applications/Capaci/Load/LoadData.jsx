a04b4d11fd7a687e2d6794c84ca7b75f1684c0cd
koreteau <cortocolloc@gmail.com>
2025-01-21 14:55:11 +0100

    Create LoadData.jsx

---
+++ app/src/components/Applications/Capaci/Load/LoadData.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoadDataCurrencyRates } from "./LoadDataCurrencyRates";
+
+export function DataLoad() {
+    return (
+        <div className="p-4">
+            <h1 className="text-lg font-bold mb-4">Currency Load</h1>
+            <div className="mt-4">
+                <LoadDataCurrencyRates />
+            </div>
+        </div>
+    );
+}
