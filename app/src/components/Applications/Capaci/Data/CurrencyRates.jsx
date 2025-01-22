fc7b2049ce527a75fa90bce75b4f57785bfa4142
koreteau <cortocolloc@gmail.com>
2025-01-22 10:03:59 +0100

    Update CurrencyRates.jsx

--- app/src/components/Applications/Capaci/Data/CurrencyRates.jsx
+++ app/src/components/Applications/Capaci/Data/CurrencyRates.jsx
@@ -167,11 +167,12 @@ export function CurrencyRates() {
 
     return (
         <>
-            <div className="flex items-center p-2 border-b gap-2 text-sm">
+            <div className="flex items-center p-2 border-b gap-2 text-xs">
                 <div className="flex gap-2">
                     <button
                         onClick={handleRefresh}
                         className="p-0.5 rounded hover:bg-gray-200"
+                        title="Refresh"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
@@ -221,7 +222,7 @@ export function CurrencyRates() {
                 <>
                     {(displayedView === "Period") ? (
                         /* Si la vue est en "Period" */
-                        <div className="h-full w-full overflow-x-auto text-sm">
+                        <div className="h-full w-full overflow-x-auto text-xs">
                             <table className="table-auto border-collapse border border-slate-500">
                                 <thead>
                                     <tr>
