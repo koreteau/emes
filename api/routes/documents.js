ec3ea6efe8516188e58ed12255478888a63fb7aa
koreteau <cortocolloc@gmail.com>
2025-03-28 16:12:02 +0100

    Create documents.js

---
+++ api/routes/documents.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { authenticateToken, isAdmin } = require("../middleware/auth");
+const {
+    createDocument,
+    getAllDocuments,
+    getDocumentById,
+    getDocumentContentById,
+    updateDocument,
+    deleteDocument
+} = require("../controllers/documentsController");
+
+const router = express.Router();
+
+// Routes accessibles uniquement aux administrateurs
+router.post("/", authenticateToken, isAdmin, createDocument); // Créer un document
+router.put("/:documentId", authenticateToken, isAdmin, updateDocument); // Modifier un document
+router.delete("/:documentId", authenticateToken, isAdmin, deleteDocument); // Supprimer un document
+
+// Routes accessibles aux utilisateurs connectés avec filtrage par permissions
+router.get("/", authenticateToken, getAllDocuments); // Récupérer tous les documents
+router.get("/:documentId", authenticateToken, getDocumentById); // Récupérer un document par ID
+router.get("/:documentId/content", authenticateToken, getDocumentContentById);
+
+
+module.exports = router;
