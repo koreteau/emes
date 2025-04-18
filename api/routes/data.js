1e8ec6954508e664b3e54c3733bead55435f1b87
koreteau <cortocolloc@gmail.com>
2025-04-18 15:43:26 +0200

    Update data.js

--- api/routes/data.js
+++ api/routes/data.js
@@ -3,20 +3,15 @@ const { authenticateToken, isAdmin } = require('../middleware/auth');
 const {
     createData,
     getAllData,
-    getDataById,
     updateData,
     deleteData,
 } = require('../controllers/dataController');
 
 const router = express.Router();
 
-// Routes accessibles uniquement aux administrateurs
-router.post('/', authenticateToken, isAdmin, createData); // Créer une entrée
-router.put('/:dataId', authenticateToken, isAdmin, updateData); // Modifier une entrée
-router.delete('/:dataId', authenticateToken, isAdmin, deleteData); // Supprimer une entrée
-
-// Routes accessibles aux utilisateurs connectés avec filtrage par sécurité
-router.get('/', authenticateToken, getAllData); // Récupérer toutes les entrées
-router.get('/:dataId', authenticateToken, getDataById); // Récupérer une entrée par ID
+router.post('/', authenticateToken, isAdmin, createData);
+router.put('/:dataId', authenticateToken, isAdmin, updateData);
+router.delete('/:dataId', authenticateToken, isAdmin, deleteData);
+router.get('/', authenticateToken, getAllData);
 
 module.exports = router;
