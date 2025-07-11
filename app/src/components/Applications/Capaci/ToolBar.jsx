72ad0c65820662864dea52478da2398165505762
koreteau <cortocolloc@gmail.com>
2025-07-12 01:12:53 +0200

    Update ToolBar.jsx

--- app/src/components/Applications/Capaci/ToolBar.jsx
+++ app/src/components/Applications/Capaci/ToolBar.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-export function ToolBar({ onRefresh, onCalculate, onRaiseData, onSave }) {
+export function ToolBar({ onRefresh, onSave, onDelete, onCalculate, onRaiseData, onRollup, onImport, onExport }) {
 	return (
-		<div className="flex items-center p-2 border-b gap-2 text-xs">
+		<div className="flex items-center p-2 border-b gap-3">
 			{onRefresh && (
 				<div className="flex gap-2">
-					<button
-						onClick={onRefresh}
-						className="p-0.5 rounded hover:bg-gray-200"
-						title="Refresh"
-					>
+					<button onClick={onRefresh} className="p-0.5 rounded hover:bg-gray-200" title="Refresh">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
 						</svg>
@@ -17,8 +13,27 @@ export function ToolBar({ onRefresh, onCalculate, onRaiseData, onSave }) {
 				</div>
 			)}
 
-			{(onCalculate || onRaiseData) && (
-				<div className="border-l-2 pl-2 flex gap-2">
+			{(onSave || onDelete) && (
+				<div className="border-l-2 pl-2 flex gap-3">
+					{onSave && (
+						<button onClick={onSave} title="Save">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+							</svg>
+						</button>
+					)}
+					{onDelete && (
+						<button onClick={onDelete} title="Delete">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+							</svg>
+						</button>
+					)}
+				</div>
+			)}
+
+			{(onCalculate || onRaiseData || onRollup) && (
+				<div className="border-l-2 pl-2 flex gap-3">
 					{onCalculate && (
 						<button onClick={onCalculate} title="Calculate">
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
@@ -28,21 +43,37 @@ export function ToolBar({ onRefresh, onCalculate, onRaiseData, onSave }) {
 					)}
 					{onRaiseData && (
 						<button onClick={onRaiseData} title="Raise Data">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
+							</svg>
+						</button>
+					)}
+					{onRollup && (
+						<button onClick={onRollup} title="Rollup">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m0-3-3-3m0 0-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75" />
 							</svg>
 						</button>
 					)}
 				</div>
 			)}
 
-			{onSave && (
-				<div className="border-l-2 pl-2 flex gap-2">
-					<button onClick={onSave} title="Save">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-						</svg>
-					</button>
+			{(onImport || onExport) && (
+				<div className="border-l-2 pl-2 flex gap-3">
+					{onImport && (
+						<button onClick={onImport} title="Import">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+							</svg>
+						</button>
+					)}
+					{onExport && (
+						<button onClick={onExport} title="Export">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+							</svg>
+						</button>
+					)}
 				</div>
 			)}
 		</div>
